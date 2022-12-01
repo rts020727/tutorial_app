f198b244726605ae2c53b66b231b1b0917cd5716
EC2 Default User <ec2-user@ip-172-31-10-226.ap-northeast-1.compute.internal>
2022-12-01 05:54:18 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  get 'signup' => 'user#new'
   
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/help' => 'static_pages#help', as: 'help'
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
   
 end
